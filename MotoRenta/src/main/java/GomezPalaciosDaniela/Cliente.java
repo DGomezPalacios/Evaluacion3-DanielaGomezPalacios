406a53f6df54169a7a9e3071a64b4ccab6988edf
DGomezPalacios <da.gomezp@duocuc.cl>
2024-11-21 16:56:27 -0300

    clases

--- MotoRenta/src/main/java/GomezPalaciosDaniela/Cliente.java
+++ MotoRenta/src/main/java/GomezPalaciosDaniela/Cliente.java
@@ -1,4 +1,20 @@
 package GomezPalaciosDaniela;
 
 public class Cliente {
+    private String rut;
+    private String nombreCompleto;
+    private String telefono;
+    private String correo;
+
+    //Metodos
+    public void agregarCliente(){
+
+    }
+
+    public void eliminarCliente(){
+
+    }
+    public void modificarCliente(){
+
+    }
 }
--- MotoRenta/src/main/java/GomezPalaciosDaniela/Motocicleta.java
+++ MotoRenta/src/main/java/GomezPalaciosDaniela/Motocicleta.java
@@ -1,4 +1,11 @@
 package GomezPalaciosDaniela;
 
 public class Motocicleta {
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int maxVelocidad;
+    private int cilindrada;
+    private int rendimiento;
+    private boolean abs;
 }
